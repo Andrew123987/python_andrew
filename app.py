bb22b7f11d63c15df39dea7e84d68c5694d4bf7c
Andrew123987 <rey-peace@mail.ru>
2024-01-21 11:12:00 +0300

    new version

--- app.py
+++ app.py
@@ -45,6 +45,20 @@ class App:
         # open home page
         wd.get("http://localhost/addressbook/addressbook/")
 
+    def add_new_contact(self, contact):
+        # add new contact
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.get("http://localhost/addressbook/addressbook/edit.php")
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("%s" % contact.contact_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("%s" % contact.contact_surname)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+
 
     def stop(self):
         self.wd.quit()
--- test_contact.py
+++ test_contact.py
@@ -1,89 +1,41 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
 from Contact import Contact
+from app import App
+import pytest
 
-class AppDynamicsJob(unittest.TestCase):
-    def setUp(self):
-        # AppDynamics will automatically override this web driver
-        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        
+
+@pytest.fixture
+def app(request):
+    fixture = App()
+    request.addfinalizer(fixture.stop)
+    return fixture
+
+
+def test_add_contact(app):
+        app.login("admin", "secret")
+        app.add_new_contact(Contact(contact_name="Andrew", contact_surname="Suvorov"))
+        app.logout()
+def test_add_empty_contact(app):
+
+        app.login("admin", "secret")
+        app.add_new_contact(Contact(contact_name="", contact_surname=""))
+        app.logout()
+
+
+
+
+
+
+
+
+
+
+
     
-    def test_add_contact(self):
-        wd = self.wd
-        self.login("secret", "admin")
-        self.add_new_contact(Contact(contact_name="Andrew", contact_surname="Suvorov"))
-        self.logout()
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.login("secret", "admin")
-        self.add_new_contact(Contact(contact_name="", contact_surname=""))
-        self.logout()
 
-
-    def open_home_page(self):
-        # open home page
-        wd = self.wd
-        wd.get("http://localhost/addressbook/addressbook/group.php?delete=Delete+group%28s%29&selected%5B%5D=7")
-
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def add_new_contact(self, contact):
-        # add new contact
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.get("http://localhost/addressbook/addressbook/edit.php")
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % contact.contact_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("%s" % contact.contact_surname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-
-    def login(self, password, username):
-        # login
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_id("LoginForm").submit()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
+
 
 
 if __name__ == "__main__":
