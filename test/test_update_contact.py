a583bcdb0fc46508a99f3e292dfc22bd09ae693b
Andrew123987 <rey-peace@mail.ru>
2024-01-23 11:12:12 +0300

    fix argument 'contact'

--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -1,4 +1,6 @@
+from model.contact import Contact
+
 def test_update_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.contact_update()
+    app.contact.contact_update(Contact(contact_name="Andrew", contact_surname="Suvorov"))
     app.session.logout()
