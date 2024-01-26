77dec3746311b8d2e7ffedfd7d3b54b3241e3bdd
Andrew123987 <rey-peace@mail.ru>
2024-01-26 14:08:57 +0300

    new fixes

--- fixture/contact_helper.py
+++ fixture/contact_helper.py
@@ -3,16 +3,21 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def contact_create(self, contact):
+    def contact_input(self, contact):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.get("http://localhost/addressbook/addressbook/edit.php")
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("%s" % contact.contact_name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
         wd.find_element_by_name("middlename").send_keys("%s" % contact.contact_surname)
+
+
+    def contact_create(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.get("http://localhost/addressbook/addressbook/edit.php")
+        ContactHelper.contact_input(self, contact)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
 
@@ -20,12 +25,7 @@ class ContactHelper:
         wd = self.app.wd
         wd.get("http://localhost/addressbook/addressbook/")
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % contact.contact_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("%s" % contact.contact_surname)
+        ContactHelper.contact_input(self, contact)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def contact_delete(self):
@@ -33,6 +33,10 @@ class ContactHelper:
         wd.get("http://localhost/addressbook/addressbook/")
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/input[2]").click()
+
+
+
+
 
 
 
