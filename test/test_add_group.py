b3dd67b327bf4d6cb389f9d07a2adf6a4076fb42
Andrew123987 <rey-peace@mail.ru>
2024-01-29 18:09:28 +0300

    update

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,9 +8,3 @@ def test_add_group(app):
 def test_add_empty_group(app):
     app.group.group_create(Group(name="", header="", footer=""))
 
-##def test_login_negative(app):
-##    app.session.login(username="X", password="X")
-
-
-
-
