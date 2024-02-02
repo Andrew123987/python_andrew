ab5cd1806d523da2267079bc4e22afe502c33293
Andrew123987 <rey-peace@mail.ru>
2024-02-02 09:14:55 +0300

    fix open_home_page()

--- fixture/session_helper.py
+++ fixture/session_helper.py
@@ -39,3 +39,8 @@ class SessionHelper:
         else:
             self.logout()
         self.login(self, username, password)
+
+    def open_home_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith('/addressbook/') and len(wd.find_elements_by_class_name('fdTableSortTrigger')) > 0):
+            wd.get("http://localhost/addressbook/addressbook/")
