389cc7b9788f15a764c063dba2fcd69c90009aca
Andrew123987 <rey-peace@mail.ru>
2024-02-03 13:30:11 +0300

    all works

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,10 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.group_create(Group(name="tst Andrew", header="tst", footer="Andrew"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
     app.group.group_create(Group(name="", header="", footer=""))
