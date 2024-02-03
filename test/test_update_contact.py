e23288555b59fe2c23e040abe351648c11e0b299
Andrew123987 <rey-peace@mail.ru>
2024-02-03 16:35:53 +0300

    all works

--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -2,6 +2,9 @@ from model.contact import Contact
 
 
 def test_update_contact(app):
+    old_contacts = app.contact.get_contact_list()
     if app.contact.count_contact() == 0:
         app.contact.contact_create(Contact(contact_name="Andrew", contact_surname="Suvorov"))
     app.contact.contact_update(Contact(contact_name="Andrew", contact_surname="Suvorov"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
