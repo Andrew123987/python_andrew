408519a95a9d6ae9a1eb8fb8e0ba95aaa0ad16eb
Andrew123987 <rey-peace@mail.ru>
2024-02-06 23:40:10 +0300

    all works

--- fixture/session_helper.py
+++ fixture/session_helper.py
@@ -34,10 +34,11 @@ class SessionHelper:
 
     def ensure_login(self, username, password):
         wd = self.app.wd
-        if self.is_logged_in_as(username):
-            return
-        else:
-            self.logout()
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
         self.login(username, password)
 
     def open_home_page(self):
