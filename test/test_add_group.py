05aee04944a58a8074e5496253c368e4050598fe
Andrew123987 <rey-peace@mail.ru>
2024-02-10 18:04:53 +0300

    all works!!!

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,17 @@
 import pytest
 from model.group import Group
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + ''
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
 
 testdata = [
-    Group(name="tst Andrew", header="tst", footer="Andrew"),
+    Group(name=random_string('name', 10), header=random_string('header', 20),
+          footer=random_string('footer', 20)),
     Group(name="", header="", footer="")
 ]
 
