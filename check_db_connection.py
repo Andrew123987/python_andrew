443f42450a848aeaaafea35d1e161e430306bff3
Andrew123987 <rey-peace@mail.ru>
2024-02-24 20:37:13 +0300

    баг

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,15 +1,16 @@
 import pymysql
 from fixture.orm import ORMFixture
 from fixture.db import DbFixture
+from model.group import Group
 
-db = DbFixture(host='127.0.0.1', name='addressbook', user='root', password='')
+db = ORMFixture(host='127.0.0.1', name='addressbook', user='root', password='')
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id='11'))
     for item in l:
         print(l)
     print(len(l))
 
 finally:
-    #pass
-    db.destroy()
+    pass
+    #db.destroy()
