105140b63a21d8e99f6480e1be2010ca7d50af44
Andrew123987 <rey-peace@mail.ru>
2024-02-25 10:27:43 +0300

    21 - баг

--- check_db_connection.py
+++ check_db_connection.py
@@ -8,7 +8,7 @@ db = ORMFixture(host='127.0.0.1', name='addressbook', user='root', password='')
 try:
     l = db.get_contacts_in_group(Group(id='11'))
     for item in l:
-        print(l)
+        print(item)
     print(len(l))
 
 finally:
--- fixture/orm.py
+++ fixture/orm.py
@@ -15,7 +15,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
-        contacts = Set(lambda: ORMFixture.ORMContact, table='address_in_groups', column='id', reverse='contacts', lazy=True)
+        contacts = Set(lambda: ORMFixture.ORMContact, table='address_in_groups', column='id', reverse='groups', lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -23,7 +23,7 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
-        groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group.id', reverse='groups', lazy=True)
+        groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
