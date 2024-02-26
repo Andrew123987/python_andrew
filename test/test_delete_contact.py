5bccc35e7e071217595ccfb097b564858d00a340
Andrew123987 <rey-peace@mail.ru>
2024-02-26 18:41:41 +0300

    задание 20

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -14,7 +14,6 @@ def test_delete_some_contact(app, db, check_ui):
     assert len(old_contacts) - 1 == len(new_contacts)
     assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                  key=Contact.id_or_max)
-    # assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         new_contacts = map(app.contact.contact_like_on_home_page, db.get_contact_list())
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
