59f375c2d44829bfc87723b156b5d8376519b9ea
Andrew123987 <rey-peace@mail.ru>
2024-02-28 09:16:35 +0300

    all works

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -15,4 +15,4 @@ def test_add_contact_to_group(app, db):
     app.contact.add_contact_to_group(app)
 
     contacts_in_group_after = db.get_contact_in_group()
-    assert len(contacts_in_group_before) < len(contacts_in_group_after)
+    assert len(contacts_in_group_before) + 1 == len(contacts_in_group_after)
