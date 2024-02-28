c1c9255db854bf400dddd9c1bfb8748c62c6beaf
Andrew123987 <rey-peace@mail.ru>
2024-02-28 13:05:58 +0300

    all works

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,13 +2,13 @@ from model.contact import Contact
 
 
 def test_add_contact(app, db, json_contacts, check_ui):
-    contact = json_contacts
     old_contacts = db.get_contact_list()
+    contact = json_contacts
     app.contact.contact_create(contact, app)
     new_contacts = db.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    assert old_contacts == new_contacts
     if check_ui:
         new_contacts = map(app.contact.contact_like_on_home_page, db.get_contact_list())
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
